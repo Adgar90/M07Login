f27ff1b200e498d99ff9536dfa5fddb72ea9e91a
Gemma <gemma.garrigosa@gmail.com>
2023-10-18 19:09:35 +0200

    Afegits arxius dimecres

--- PRACTICA4Gemma/views/userLogin.php
+++ PRACTICA4Gemma/views/userLogin.php
@@ -2,22 +2,13 @@
 
 include('db_connection.php');
 
-if (isset($_POST['send'])){
+
     $pass = $_POST['passwd'];
     $email = $_POST['email'];
-    $actiu = $_POST['active'];
-
-    if ($actiu = 'si'){
-        $actiu = 'True';
-    }else{
-        $actiu = 'False';
-    }
-
-    echo "$actiu";
+  
     //Fem consulta query
     $consulta = SELECT * FROM `users` WHERE `email` = '$email';
  
-    $paraularandom = 'hola';
     //Introduim les dades de consulta a la taula user
     $result = mysqli_query($conn, $consulta);
     var_dump($result);
@@ -27,6 +18,6 @@ if (isset($_POST['send'])){
     //     die("Query fail!");
     // }
 
-    //header("Location: usuari.php");    
-}
+    header("Location: usuari.php");    
+
 ?>
