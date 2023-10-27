e8149f29d30ae13b831f8f77cdddcb323ca21b1f
Adria <adriagp90@gmail.com>
2023-10-27 19:33:06 +0200

    afegides views usuari en 'es'

---
+++ PRACTICA5ADRIA/views/user_es.php
@@ -0,0 +1,33 @@
+<?php
+    include("../login/userLogin.php");
+    
+     
+    $user = $resultat->fetch_assoc();
+    if($user != null) { 
+        if ($user["rol"] == "professorat") {
+            echo "<p>Hola ".$user["name"]." eres profesor!!</p>";
+            echo "<p>La lista de usuarios de la base de datos es:</p>";
+            foreach (selectAll() as $value) {
+                echo "nombre y apellido: ".$value["name"]." ".$value["surname"]."<br>";
+            }
+            
+        } else {
+            echo "<p><strong>soy un alumno<br>";
+            echo "nombre: ".$user["name"]."<br>";
+            echo "apellido: ".$user["surname"]."<br>";
+            echo "email: ".$user["email"]."</strong></p>";
+        }
+    } else {
+        include("login_es.html"); 
+        echo "Login incorrecto<br>";
+    }
+
+    echo '<a href="../idioma.php">Idioma</a><br>';
+    echo '<a href="../borraCookie.php">Borrar cookie</a>';
+    function selectAll() {
+        include("../conn/db_connection.php");
+        $select = "SELECT * FROM `user`;";
+        $resposta = mysqli_query($conn, $select);
+        return $resposta->fetch_all(MYSQLI_ASSOC);
+    }
+?>
