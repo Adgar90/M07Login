925ed2c51efec566074f5470b94ce8f00b3d27d0
Gemma <gemma.garrigosa@gmail.com>
2023-11-03 17:09:50 +0100

    Afegits arxius de connexió amb la bbdd

---
+++ PRACTICA6Gemma/dbConf.php
@@ -0,0 +1,12 @@
+<?php
+
+// Guardem 4 dades necessaries per fer la connexió amb la BBDD del servidor
+// $db_host = "localhost";
+// $db_nombre = "users";
+// $db_usuario = "root";
+// $db_passwd = "";
+define("db_host","localhost");
+define("db_nombre","users");
+define("db_usuario","root");
+define('db_passwd','');
+?>
